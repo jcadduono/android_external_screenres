6527a824eba0fa0ad81f9fb0ebae349c54998fce
James Christopher Adduono <jc@adduono.com>
2016-10-19 01:39:47 -0400

    Use API 21
    Change-Id: I21b360f228b9b99e8586e134be6ec9322fe8cb97

--- Android.mk
+++ Android.mk
@@ -3,6 +3,5 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := screenres
 LOCAL_SRC_FILES := screenres.c
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
+LOCAL_SDK_VERSION := 21
 include $(BUILD_EXECUTABLE)
